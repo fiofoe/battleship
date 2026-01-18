d9ad1f7579e74064549c09bb7dd5d4b3be1b4e7f
fiofoe <156612118+fiofoe@users.noreply.github.com>
2026-01-18 02:25:44 +0100

    first draft shoot func
    Added ShotFired event and implemented shoot function logic.

--- Battleship.sol
+++ Battleship.sol
@@ -29,6 +29,52 @@ contract Battleship{
     address public player2;
     address public currentTurn;
     address public winner;
+//events
 
+    event ShotFired(
+    address indexed shooter,
+    uint8 x,
+    uint8 y,
+    bool hit,
+    bool sunk
+    );
 
+// functions
+    function shoot(uint8 x, uint8 y) external returns (bool hitResult) {
+        require(
+            (msg.sender == player1 && currentTurn == player1) ||
+            (msg.sender == player2 && currentTurn == player2), 
+            "It is not your turn"
+        );
+        require(x < 10 && y < 10, "Invalid coordinates");
+
+        //determine board
+        CellState[10][10] storage opponentBoard = (msg.sender == player1) ? boardPlayer2 : boardPlayer1;
+
+        //prevent double shooting
+        require(opponentBoard[x][y] != CellState.ShipHit,"Already shot here");
+
+        bool isSunk = false;
+        bool isHit = false;
+
+        if (opponentBoard[x][y] == CellState.Ship) {
+            isHit = true;
+            opponentBoard[x][y] = CellState.ShipHit;
+            isSunk = !(
+                (x > 0 && opponentBoard[x - 1][y] == CellState.Ship) || // check left
+                (x < 9 && opponentBoard[x + 1][y] == CellState.Ship) || // check right
+                (y > 0 && opponentBoard[x][y - 1] == CellState.Ship) || // check above
+                (y < 9 && opponentBoard[x][y + 1] == CellState.Ship) // check below
+             );
+        }
+
+        emit ShotFired(msg.sender, x, y, isHit, isSunk);
+
+        // switch turns only if miss
+        if (!isHit) {
+            currentTurn = (currentTurn == player1) ? player2 : player1;
+        }
+
+        return isHit;
+    }
 }
