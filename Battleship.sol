85a97af10f6f3b7977dcf11a2a6601ba749b1c39
McBagel <lorenz.holz@gmx.de>
2026-01-19 21:36:30 +0100

    Added placement function, small tweaks to allow dynamic board

--- Battleship.sol
+++ Battleship.sol
@@ -3,11 +3,16 @@ pragma solidity >=0.8.2 <0.9.0;
 contract Battleship{
 
     enum CellState { Empty, Ship, ShipHit }
-    CellState[10][10] public boardPlayer1; //Schiffe von Player 1, Player 2 schiesst drauf
-    CellState[10][10] public boardPlayer2; //Schiffe von Player 2, Player 1 schiesst drauf
+    CellState[][] public boardPlayer1; //Schiffe von Player 1, Player 2 schiesst drauf
+    CellState[][] public boardPlayer2; //Schiffe von Player 2, Player 1 schiesst drauf
+    uint8 public dimension = 10;
+    uint8[] public shipsizes = [5, 4, 3, 3, 2];
+    GameState public phase = GameState.Placements;
+
     enum GameState{
         Created,
         WaitingForPlayer2,
+        Placements,
         Player1Turn,
         Player2Turn,
         GameOver
@@ -40,16 +45,17 @@ contract Battleship{
     );
 
 // functions
+
     function shoot(uint8 x, uint8 y) external returns (bool hitResult) {
         require(
             (msg.sender == player1 && currentTurn == player1) ||
             (msg.sender == player2 && currentTurn == player2), 
             "It is not your turn"
         );
-        require(x < 10 && y < 10, "Invalid coordinates");
+        require(x < dimension && y < dimension, "Invalid coordinates");
 
         //determine board
-        CellState[10][10] storage opponentBoard = (msg.sender == player1) ? boardPlayer2 : boardPlayer1;
+        CellState[][] storage opponentBoard = (msg.sender == player1) ? boardPlayer2 : boardPlayer1;
 
         //prevent double shooting
         require(opponentBoard[x][y] != CellState.ShipHit,"Already shot here");
@@ -77,4 +83,86 @@ contract Battleship{
 
         return isHit;
     }
+
+
+    function place(uint8  [][] calldata placements) external returns (bool valid){ //definiert nur für player 1
+        require(phase ==GameState.Placements, "No changes to Placements are accepted at this time");
+        CellState[][] memory board = new CellState[][](dimension);
+        for(uint8 col=0; col<dimension; col++){
+                CellState [] memory column = new CellState[] (dimension);
+                for(uint8 row =0; row<dimension; row++){
+                    column[row]=CellState.Empty;
+                }
+                board[col] = column;
+            }
+        for(uint8 i=0; i< placements.length; i++){
+            
+            uint8 x = placements[i][0];
+            uint8 y = placements[i][1];
+            uint8 direction = placements[i][2];
+            uint8 length = shipsizes[i];
+            
+
+
+            require(x<dimension && y<dimension && x>=0 && y>=0, "Invalid placement");
+            if(direction == 0){
+                require(x+length-1<dimension, "Invalid Placement");
+                if(x>0){
+                    require(board[x-1][y]!=CellState.Ship, "Adjacent placings are prohibeted");
+                }
+                if(x<dimension-length){
+                    require(board[x+length][y]!=CellState.Ship, "Adjacent placings are prohibeted");
+                }
+                
+                for(uint8 j=0; j<length; j++){
+                    require(board[x+j][y]!=CellState.Ship, "Conflict");
+                    if(y>0)
+                    {
+                        require(board[x+j][y-1]!=CellState.Ship, "Adjacent placings are prohibeted");
+                    }
+                    if(y<dimension-1)
+                    {
+                        require(board[x+j][y+1]!=CellState.Ship, "Adjacent placings are prohibeted");
+                    }
+                    board[x+j][y]= CellState.Ship;
+                }
+            }
+
+           if(direction == 1){
+                require(y+length-1<dimension, "Invalid Placement");
+                if(y>0){
+                    require(board[x][y-1]!=CellState.Ship, "Adjacent placings are prohibeted");
+                }
+                if(y<dimension-length){
+                    require(board[x][y+length]!=CellState.Ship, "Adjacent placings are prohibeted");
+                }
+                
+                for(uint8 j=0; j<length; j++){
+                    require(board[x][y+j]!=CellState.Ship, "Conflict");
+                    if(x>0)
+                    {
+                        require(board[x-1][y+j]!=CellState.Ship, "Adjacent placings are prohibeted");
+                    }
+                    if(x<dimension-1)
+                    {
+                        require(board[x+1][y+j]!=CellState.Ship, "Adjacent placings are prohibeted");
+                    }
+                    board[x][y+j]= CellState.Ship;
+                }
+            }
+        
+        }
+        if(msg.sender == player1)
+        {
+            boardPlayer1 = board;
+            return true;
+        }
+        else if(msg.sender == player2)
+        {
+            boardPlayer2 = board;
+            return true;
+        }
+        require(false, "You are not a Player in this game");
+    }
+    
 }
